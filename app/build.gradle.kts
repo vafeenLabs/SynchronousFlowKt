2c7e2cd7e139f210bca7d4a4452e232427c56653
Vafeen <666av6@gmail.com>
2024-12-18 02:10:43 +0300

    Add lib initial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-
+    implementation(project(":lib"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
